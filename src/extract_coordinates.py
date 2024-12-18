cf85b41f124266eca6f7270f50b8046fb40b8d8f
Julius-Schmulius <halankjulius@gmail.com>
2024-12-18 18:58:36 +0100

    env file,bounding box limit, coordinate changes

--- src/extract_coordinates.py
+++ src/extract_coordinates.py
@@ -3,16 +3,16 @@ import pandas as pd
 from shapely.geometry import box
 import os
 from pathlib import Path
-#import folium
+import folium
 #https://pypi.org/project/flickrapi/
 #https://geopandas.org/en/stable/getting_started/install.html
 
 project_dir = Path(__file__).parent.parent
 shapefile_path = os.path.join(project_dir, "in", "1976-2000.shp")
 shapefile = gpd.read_file(shapefile_path)
-
+print(shapefile.head())
 print(f"Current CRS: {shapefile.crs}") # current Coordinate Reference System
-
+#print(map_df.columns.tolist())
 # re-project to Web Mercator to convert lat/long -> degrees
 if shapefile.crs.is_geographic: # is.geographic checks if coodinates are in lat/long
     projected_shapefile = shapefile.to_crs({'proj':'cea'})  # 3857 = Web Mercator
@@ -22,9 +22,9 @@ else:
 
 print(projected_shapefile.crs)
 print("area: " + str(max(projected_shapefile['geometry'].area / 10 ** 6)) + " km²")
-"""
+
 # filter for specific climate types
-filtered_df = projected_shapefile[projected_shapefile['climate'].isin(["BWh Arid-Desert-Hot", "BWk Arid-Desert-Cold"])]
+filtered_df = projected_shapefile[projected_shapefile['GRIDCODE'].isin([21, 22])] #TODO: argc argv
 
 # Debugging: Print the number of rows after filtering by climate type
 #print(f"Number of rows after filtering by climate type: {len(filtered_df)}")
@@ -44,17 +44,17 @@ filtered_df = filtered_df[filtered_df.is_valid]
 #print(f"Number of rows after ensuring valid geometries: {len(filtered_df)}")
 
 # calculate areas in km^2 
-filtered_df['area_km2'] = filtered_df.geometry.area / 1e6  # leftshift by 6 to convert to km^2
+filtered_df['area_km²'] = filtered_df['geometry'].area / 1e6  # leftshift by 6 to convert to km^2
 # Debugging: Print the areas to check if they are calculated correctly
-#print(filtered_df[['climate', 'area_km2']])
+print(filtered_df[['GRIDCODE', 'area_km²']])
 
-filtered_df = filtered_df[(filtered_df['area_km2'] >= 5000)] # filter by size
+filtered_df = filtered_df[(filtered_df['area_km²'] >= 5000)] # filter by size
 
 # Debugging: Print the number of rows after filtering by area
 #print(f"Number of rows after filtering by area: {len(filtered_df)}")
 
 # re-project the filtered geometries to WGS84 to project in the Folium map
-filtered_df = filtered_df.to_crs(epsg=4326)
+filtered_df = filtered_df.to_crs(epsg=4004) #base map projection, same as 4326
 
 # create dataframe to store bounding box values
 bounding_boxes = []
@@ -79,8 +79,7 @@ output_dir = os.path.join(os.getcwd(), "output")
 os.makedirs(output_dir, exist_ok=True)
 output_csv_path = os.path.join(output_dir, "bounding_boxes.csv")
 bounding_boxes_df.to_csv(output_csv_path, index=False)
-"""
-"""
+
 # create a Folium map centered on the averaged bbox coordinates
 center_lat = bounding_boxes_df[['ymin', 'ymax']].mean().mean()
 center_lon = bounding_boxes_df[['xmin', 'xmax']].mean().mean()
@@ -97,4 +96,3 @@ for bbox in bounding_boxes_df.itertuples():
 # save map as html file in "output" subdir
 output_html_path = os.path.join(output_dir, "bounding_boxes_map.html")
 m.save(output_html_path)
-"""
